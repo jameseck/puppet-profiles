7aed144ab4cb8d2454acde8396fe4f28dcbf03ea
James Eckersall <james.eckersall@gmail.com>
2015-03-18 19:42:37 +0000

    added mandatory_packages profile;

--- manifests/base.pp
+++ manifests/base.pp
@@ -7,6 +7,7 @@ class profiles::base (
   notify { 'This is the profiles::base class': }
 
   include '::profiles::jeuser'
+  include '::profiles::mandatory_packages'
   include '::ssh'
   include '::ntp'
 
---
+++ manifests/mandatory_packages.pp
@@ -0,0 +1,72 @@
+class profiles::mandatory_packages (
+) {
+
+  anchor { 'profiles::mandatory_packages::begin': }
+
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystemmajrelease {
+        /^5.*/: {
+          $packages = [
+            'augeas',
+            'strace',
+            'yum-utils',
+            'tcpdump',
+            'screen',
+            'sysstat',
+            'vim-enhanced',
+            'dmidecode',
+            'telnet',
+            'wget',
+            'curl',
+            'policycoreutils',
+            'bash-completion',
+          ]
+        }
+        /^[67].*/: {
+          $packages = [
+            'augeas',
+            'strace',
+            'yum-utils',
+            'tcpdump',
+            'screen',
+            'sysstat',
+            'vim-enhanced',
+            'dmidecode',
+            'telnet',
+            'wget',
+            'curl',
+            'policycoreutils-python',
+            'bash-completion',
+            'ipmitool',
+          ]
+        }
+        default: { fail("operatingsystemmajrelease ${::operatingsystemmajrelease} not supported") }
+      }
+    }
+    'Debian': {
+      $packages = [
+        'augeas-tools',
+        'strace',
+        'tcpdump',
+        'screen',
+        'sysstat',
+        'vim-nox',
+        'dmidecode',
+        'telnet',
+        'wget',
+        'curl',
+        'bash-completion',
+        'ipmitool',
+      ]
+    }
+    default: { fail("osfamily ${::osfamily} is not supported.") }
+  }
+
+  package { $packages:
+    ensure => installed,
+  }
+
+  anchor { 'profiles::mandatory_packages::end': }
+
+}
