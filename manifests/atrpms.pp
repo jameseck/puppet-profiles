0800f6215fa74c6b084c677a267b7f0b20c6c6ca
James Eckersall <james.eckersall@gmail.com>
2015-03-30 16:54:49 +0100

    fixed atrpms osrelease var

--- manifests/atrpms.pp
+++ manifests/atrpms.pp
@@ -14,36 +14,36 @@ class profiles::atrpms (
   case $::osfamily {
     'RedHat': {
       yumrepo { 'atrpms':
-        baseurl     => "http://dl.atrpms.net/el${::os_maj_version}-${::architecture}/atrpms/stable",
+        baseurl     => "http://dl.atrpms.net/el${::operatingsystemmajrelease}-${::architecture}/atrpms/stable",
         proxy       => $atrpms::params::proxy,
         enabled     => $atrpms_enable,
         exclude     => $atrpms_exclude,
         includepkgs => $atrpms_include,
         gpgcheck    => '1',
         gpgkey      => 'http://packages.atrpms.net/RPM-GPG-KEY.atrpms',
-        descr       => "ATrpms packages for Enterprise Linux ${::os_maj_version} - ${::architecture}",
+        descr       => "ATrpms packages for Enterprise Linux ${::operatingsystemmajrelease} - ${::architecture}",
       }
 
       yumrepo { 'atrpms-testing':
-        baseurl     => "http://dl.atrpms.net/el${::os_maj_version}-${::architecture}/atrpms/testing",
+        baseurl     => "http://dl.atrpms.net/el${::operatingsystemmajrelease}-${::architecture}/atrpms/testing",
         proxy       => $atrpms::params::proxy,
         enabled     => $testing_enable,
         exclude     => $testing_exclude,
         includepkgs => $testing_include,
         gpgcheck    => '1',
         gpgkey      => 'http://packages.atrpms.net/RPM-GPG-KEY.atrpms',
-        descr       => "ATrpms testing packages for Enterprise Linux ${::os_maj_version} - ${::architecture}",
+        descr       => "ATrpms testing packages for Enterprise Linux ${::operatingsystemmajrelease} - ${::architecture}",
       }
 
       yumrepo { 'atrpms-bleeding':
-        baseurl     => "http://dl.atrpms.net/el${::os_maj_version}-${::architecture}/atrpms/bleeding",
+        baseurl     => "http://dl.atrpms.net/el${::operatingsystemmajrelease}-${::architecture}/atrpms/bleeding",
         proxy       => $atrpms::params::proxy,
         enabled     => $bleeding_enable,
         exclude     => $bleeding_exclude,
         includepkgs => $bleeding_include,
         gpgcheck    => '1',
         gpgkey      => 'http://packages.atrpms.net/RPM-GPG-KEY.atrpms',
-        descr       => "ATrpms bleeding edge packages for Enterprise Linux ${::os_maj_version} - ${::architecture}",
+        descr       => "ATrpms bleeding edge packages for Enterprise Linux ${::operatingsystemmajrelease} - ${::architecture}",
       }
     }
     default: { fail("${::osfamily} is not supported.") }
