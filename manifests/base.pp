d8164f9b73171d7c53a9a9a5199fb1749c342b22
James Eckersall <james.eckersall@gmail.com>
2015-09-11 06:08:36 +0100

    ssh client options

--- manifests/base.pp
+++ manifests/base.pp
@@ -7,10 +7,19 @@ class profiles::base (
   include '::profiles::jeuser'
   include '::profiles::mandatory_packages'
   include '::profiles::puppet'
-  include '::ssh'
   include '::ntp'
   include '::sudo'
   include '::timezone'
+  include '::ssh'
+
+  ::ssh::client::config::user { 'root':
+    ensure              => present,
+    user_home_dir       => '/root',
+    manage_user_ssh_dir => false,
+    options             => {
+      'AgentForwarding' => 'yes'
+    }
+  }
 
 #  if ( $::virtual == 'kvm' ) {
 #    include '::profiles::kvm_guest'
